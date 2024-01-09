b193573055e3d59720563f49bf50bb891a70658d
Snêu <hoangph271@gmail.com>
2024-01-10 01:06:35 +0700

    Refactor MaterialApp configuration

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -22,14 +22,7 @@ class SaneGallery extends StatelessWidget {
       listenable: Listenable.merge([settingsController, gifsController]),
       builder: (BuildContext context, Widget? _) {
         return MaterialApp.router(
-          // * Providing a restorationScopeId allows the Navigator built by the
-          // * MaterialApp to restore the navigation stack when a user leaves and
-          // * returns to the app after it has been killed while running in the
-          // * background.
           restorationScopeId: 'app',
-          // * Provide the generated AppLocalizations to the MaterialApp. This
-          // * allows descendant Widgets to display the correct translations
-          // * depending on the user's locale.
           localizationsDelegates: const [
             AppLocalizations.delegate,
             GlobalMaterialLocalizations.delegate,
@@ -39,14 +32,11 @@ class SaneGallery extends StatelessWidget {
           supportedLocales: const [
             Locale('en', ''),
           ],
-
           onGenerateTitle: (BuildContext context) =>
               AppLocalizations.of(context)!.appTitle,
-
           theme: ThemeData(),
           darkTheme: ThemeData.dark(),
           themeMode: settingsController.themeMode,
-
           routerConfig: routerConfig(
             settingsController,
             gifsController,
