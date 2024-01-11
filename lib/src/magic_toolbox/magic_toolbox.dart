9fcc30a0c95557be5982cf59850b7fd32dc9219e
Snêu <hoangph271@gmail.com>
2024-01-12 01:02:50 +0700

    Add share functionality to MagicToolbox

--- lib/src/magic_toolbox/magic_toolbox.dart
+++ lib/src/magic_toolbox/magic_toolbox.dart
@@ -7,6 +7,7 @@ import 'package:sane_gallery/src/widgets/fancy_elevated_button.dart';
 import 'package:sane_gallery/src/widgets/instax_card.dart';
 import 'package:sane_gallery/src/widgets/mobile_only_widget.dart';
 import 'package:sane_gallery/src/widgets/sane_padding.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:widgets_to_image/widgets_to_image.dart';
 
 class MagicToolbox extends StatefulWidget {
@@ -30,29 +31,37 @@ class _MagicToolboxState extends State<MagicToolbox> {
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FancyElevatedButton(
-              label: const Text(
-                'Instax Printer...!',
-                style: TextStyle(fontStyle: FontStyle.italic),
-              ),
-              onPressed: () async {
-                final ImagePicker picker = ImagePicker();
-                final image =
-                    await picker.pickImage(source: ImageSource.gallery);
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: FancyElevatedButton(
+                label: const Text(
+                  'Instax Printer...!',
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                ),
+                onPressed: () async {
+                  final ImagePicker picker = ImagePicker();
+                  final image =
+                      await picker.pickImage(source: ImageSource.gallery);
 
-                if (image != null) {
-                  setState(() {
-                    _imageBytes = image.readAsBytes();
-                  });
-                }
-              },
-              icon: const Icon(Icons.add_a_photo),
+                  if (image != null) {
+                    setState(() {
+                      _imageBytes = image.readAsBytes();
+                    });
+                  }
+                },
+                icon: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(Icons.add_a_photo),
+                ),
+              ),
             ),
             if (_imageBytes != null)
               FutureBuilder(
                   future: _imageBytes,
                   builder: (context, snapshot) {
                     if (snapshot.hasData) {
+                      final bytes = snapshot.data as Uint8List;
+
                       return Column(
                         children: [
                           WidgetsToImage(
@@ -60,9 +69,8 @@ class _MagicToolboxState extends State<MagicToolbox> {
                             child: SizedBox(
                               width: 300,
                               height: 300,
-                              child: InstaxCard(
-                                  imageProvider:
-                                      MemoryImage(snapshot.data as Uint8List)),
+                              child:
+                                  InstaxCard(imageProvider: MemoryImage(bytes)),
                             ),
                           ),
                           SanePadding(
@@ -73,10 +81,23 @@ class _MagicToolboxState extends State<MagicToolbox> {
                                   onPressed: _saveInstaxPng,
                                   icon: const Icon(Icons.download_sharp),
                                 ),
+                                const SizedBox(width: 16),
                                 MobileOnlyWidget(
                                     child: IconButton.outlined(
                                   icon: const Icon(Icons.share),
-                                  onPressed: () {},
+                                  onPressed: () async {
+                                    final bytes = await widgetsToImageController
+                                        .capture();
+
+                                    Share.shareXFiles([
+                                      XFile.fromData(
+                                        bytes!,
+                                        name:
+                                            'saneGallery ${DateTime.now().toIso8601String()}.png',
+                                        mimeType: 'image/png',
+                                      ),
+                                    ]);
+                                  },
                                 )),
                               ],
                             ),
@@ -103,7 +124,6 @@ class _MagicToolboxState extends State<MagicToolbox> {
       ext: 'png',
     );
 
-    // FIXME: The download directory is sandboxed, so we can't access it.
     print(res);
   }
 }
