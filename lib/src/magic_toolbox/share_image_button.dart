66f5e2de0ff9216b5827f1d43477419252353638
Snêu <hoangph271@gmail.com>
2024-01-17 13:56:43 +0700

    Refactor ShareImageButton to use StatefulWidget

--- lib/src/magic_toolbox/share_image_button.dart
+++ lib/src/magic_toolbox/share_image_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:widgets_to_image/widgets_to_image.dart';
 
-class ShareImageButton extends StatelessWidget {
+class ShareImageButton extends StatefulWidget {
   const ShareImageButton({
     super.key,
     required this.widgetsToImageController,
@@ -11,19 +11,38 @@ class ShareImageButton extends StatelessWidget {
   final WidgetsToImageController widgetsToImageController;
 
   @override
+  State<ShareImageButton> createState() => _ShareImageButtonState();
+}
+
+class _ShareImageButtonState extends State<ShareImageButton> {
+  var _isSharing = false;
+
+  @override
   Widget build(BuildContext context) {
     return IconButton.outlined(
       icon: const Icon(Icons.share),
-      onPressed: () async {
-        final bytes = await widgetsToImageController.capture();
+      onPressed: _isSharing
+          ? null
+          : () async {
+              try {
+                setState(() {
+                  _isSharing = true;
+                });
 
-        Share.shareXFiles([
-          XFile.fromData(
-            bytes!,
-            name: 'saneGallery ${DateTime.now().toIso8601String()}.png',
-            mimeType: 'image/png',
-          ),
-        ]);
+                final bytes = await widget.widgetsToImageController.capture();
+
+                await Share.shareXFiles([
+                  XFile.fromData(
+                    bytes!,
+                    name: 'saneGallery ${DateTime.now().toIso8601String()}.png',
+                    mimeType: 'image/png',
+                  ),
+                ]);
+              } finally {
+                setState(() {
+                  _isSharing = false;
+                });
+              }
       },
     );
   }
