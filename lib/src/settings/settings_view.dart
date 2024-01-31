002271ef29d86872ba36f2ff374c5426aae93441
Phan Huy Hoang <hoangph271@gmail.com>
2024-01-31 14:59:16 +0700

    Refactor settings view layout

--- lib/src/settings/settings_view.dart
+++ lib/src/settings/settings_view.dart
@@ -18,91 +18,97 @@ class SettingsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(maxWidth: 600),
-        child: Column(
-          children: [
-            SanePadding(
-              child: ToggleThemeButtons(settingsController: settingsController),
-            ),
-            OrientationBuilder(builder: (context, orientation) {
-              return SanePadding(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return LayoutBuilder(builder: (context, constraints) {
+      final aspectRatio = constraints.maxWidth / constraints.maxHeight;
+      final isVertical = aspectRatio < 1;
+
+      return SanePadding(
+        child: Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 600),
+            child: Column(
+              children: [
+                SanePadding(
+                  child: ToggleThemeButtons(
+                      settingsController: settingsController),
+                ),
+                Flex(
+                  direction: isVertical ? Axis.vertical : Axis.horizontal,
                   children: [
-                    Text(
-                      'Orientation: ${orientation.toString().split('.').last}',
-                      style: Theme.of(context).textTheme.bodyMedium,
+                    Expanded(
+                      flex: isVertical ? 0 : 1,
+                      child: TextField(
+                        readOnly: true,
+                        controller: TextEditingController(
+                            text: settingsController.apiRoot),
+                        decoration: const InputDecoration(
+                          labelText: 'API Root',
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.url,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                      height: 16,
+                    ),
+                    Expanded(
+                      flex: isVertical ? 0 : 1,
+                      child: TextField(
+                        readOnly: true,
+                        controller: TextEditingController(
+                            text: settingsController.apiKey),
+                        decoration: const InputDecoration(
+                          labelText: 'API Key',
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.text,
+                      ),
                     ),
                   ],
                 ),
-              );
-            }),
-            SanePadding(
-              child: TextField(
-                readOnly: true,
-                controller:
-                    TextEditingController(text: settingsController.apiRoot),
-                decoration: const InputDecoration(
-                  labelText: 'API Root',
-                  border: OutlineInputBorder(),
-                ),
-                keyboardType: TextInputType.url,
-              ),
-            ),
-            SanePadding(
-              child: TextField(
-                readOnly: true,
-                controller:
-                    TextEditingController(text: settingsController.apiKey),
-                decoration: const InputDecoration(
-                  labelText: 'API Key',
-                  border: OutlineInputBorder(),
-                ),
-                keyboardType: TextInputType.text,
-              ),
-            ),
-            SanePadding(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Page size: ${settingsController.pageSize}',
-                    style: Theme.of(context).textTheme.bodyMedium,
+                SanePadding(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Page size: ${settingsController.pageSize}',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      Slider(
+                        min: 8,
+                        max: 24,
+                        divisions: 4,
+                        label: settingsController.pageSize.toString(),
+                        onChanged: (value) {
+                          settingsController.updatePageSize(value.toInt());
+                        },
+                        value: settingsController.pageSize.toDouble(),
+                      ),
+                    ],
                   ),
-                  Slider(
-                    min: 8,
-                    max: 24,
-                    divisions: 4,
-                    label: settingsController.pageSize.toString(),
-                    onChanged: (value) {
-                      settingsController.updatePageSize(value.toInt());
+                ),
+                SanePadding(
+                    child: UnconstrainedBox(
+                  child: FancyElevatedButton(
+                    label: const Text('Magic Toolbox'),
+                    onPressed: () {
+                      context.go(MagicToolbox.pathName);
                     },
-                    value: settingsController.pageSize.toDouble(),
+                    icon: SvgPicture.asset(
+                      'assets/svg/magic-wand.svg',
+                      width: 24,
+                      height: 24,
+                      colorFilter: ColorFilter.mode(
+                          Theme.of(context).primaryColor, BlendMode.srcIn),
+                    ),
                   ),
-                ],
-              ),
+                )),
+              ],
             ),
-            SanePadding(
-                child: UnconstrainedBox(
-              child: FancyElevatedButton(
-                label: const Text('Magic Toolbox'),
-                onPressed: () {
-                  context.go(MagicToolbox.pathName);
-                },
-                icon: SvgPicture.asset(
-                  'assets/svg/magic-wand.svg',
-                  width: 24,
-                  height: 24,
-                  colorFilter: ColorFilter.mode(
-                      Theme.of(context).primaryColor, BlendMode.srcIn),
-                ),
-              ),
-            )),
-          ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
